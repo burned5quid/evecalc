4c808e323dd2abc2426e690fea8a692b989bd08b
Mick Cooney <mcooney@kaybensoft.com>
2013-06-10 00:56:42 +0100

    refs #2, #3 - Started working on the construction cost calculations

--- R/construction.cost.R
+++ R/construction.cost.R
@@ -86,17 +86,17 @@ calculate.construction.cost <- function(typeID, ME = 0, price.dt = pricedata.dt,
 calculate.advanced.construction.cost <- function(typeID, ME = c(0, 0), price.dt, verbose = FALSE, dbconnect = data.connection) {
 
     calc.cost <- function(typeID) {
-        mat.1.dt <- calculate.construction.cost(typeID,          ME = ME[1], price.dt = price.dt, verbose = TRUE, dbconnect = dbconnect);
-        mat.2.dt <- calculate.construction.cost(mat.1.dt$typeID, ME = ME[2], price.dt = price.dt, verbose = TRUE, dbconnect = dbconnect);
+        mat.1.dt <- calculate.construction.cost(typeID,             ME = ME[1], price.dt = price.dt, verbose = TRUE, dbconnect = dbconnect);
+        mat.2.dt <- calculate.construction.cost(mat.1.dt$matTypeID, ME = ME[2], price.dt = price.dt, verbose = TRUE, dbconnect = dbconnect);
 
-        mat.1.dt <- within(mat.1.dt, { mergeID = typeID });
-        mat.2.dt <- within(mat.2.dt, { mergeID = constructTypeID });
-
-        basic.dt <- mat.1.dt[!typeID %in% mat.2.dt$constructTypeID];
+        mat.1.dt[, mergeID := matTypeID];
+        mat.2.dt[, mergeID := typeID];
+print(mat.1.dt); print(mat.2.dt);
+        basic.dt <- mat.1.dt[!matTypeID %in% mat.2.dt$typeID];
         comp.dt  <- merge(mat.1.dt[, list(mergeID, q1 = required)],
                           mat.2.dt[, list(mergeID, typeID, typeName, q2 = required, price)], by = 'mergeID');
 
-        comp.dt <- within(comp.dt, { quantity = q1 * q2 });
+        comp.dt[, quantity := q1 * q2];
 
         cost.dt <- rbind(basic.dt[, list(typeID, typeName, quantity = required, price)],
                          comp.dt [, list(typeID, typeName, quantity, price)]);
